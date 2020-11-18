91cca9d789d1ed7eb598f57eed8381d460adf275
Garcia McGee <agarciamcge@chsmedical.com>
2020-11-18 12:50:26 -0700

    commit on master

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect} from 'react';
+import axios from 'axios'
+
+const URL = 'https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui=1092422'
+
+const Table = () => {
+  const [interactions, setInteractions] = useState([])
+
+  useEffect(() => {
+    getInteractions()
+  }, [])
+
+  const getInteractions = async () => {
+    const response = await axios.get(URL)
+    setInteractions(response.data)
+  }
+
+const renderHeader = () => {
+  let headerElement = ['Name', 'Severity', 'Description', 'URL']
+
+  return headerElement.map((key, index) => {
+  return <th key={index}>{key.toUpperCase()}</th>
+  })
+}
+
+const renderBody = () => {
+  return interactions && interactions.map(({item, index}) => { 
+    const {interactionTypeGroup[0].interactionType[0].minConceptItem} = item
+    return (  
+          
+      <tr key={index.rxcui}>
+        <td>{item.rxcui}</td>
+        <td>{item.name}</td>
+        <td>{item.tty}</td>        
+      </tr>
+    )
+  })
+}
+
+return(
+  <>
+  <h1 id='title'> Drug Interaction Table</h1>
+  <table id='interaction'>
+    <thread> 
+      <tr>{renderHeader()}</tr>
+    </thread>
+      <tbody>
+        {renderBody()}
+      </tbody>    
+  </table>
+  </>
+)
+}
+
+export default Table
